3afd6540a15e47f7d8e8a82b5fbdcf10563a9f1b
KenWatanabe1520 <126440591+KenWatanabe1520@users.noreply.github.com>
2023-04-09 13:49:13 +0000

    Finish Kadai4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @book_comment = BookComment.new
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
   devise_for :users
   resources :books do
-    resource :favorites, only[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
